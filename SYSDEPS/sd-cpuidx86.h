d619c3d1d4288b6ee70849fb74b6050077e4ffc3
m0 <m0@viper.internal.network>
2009-11-10 15:02:15 +0000

    Last removal

--- SYSDEPS/sd-cpuidx86.h
+++
@@ -1,9 +0,0 @@
-#ifndef SD_CPUIDX86_H
-#define SD_CPUIDX86_H
-
-#include "sd-sysinfo.h"
-
-#define SD_CPUIDX86_ARCH   SD_SYSINFO_ARCH_X86_64
-#define SD_CPUIDX86_CC_TYPE SD_SYSINFO_CC_TYPE_GCC
-
-#endif
